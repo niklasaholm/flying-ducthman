bfd206ba345a658938a2f436f45c2c0f98a12796
niklasaholm <niklas.aholm@outlook.com>
2016-02-23 21:24:57 +0100

    add search

--- filter.js
+++ filter.js
@@ -3,11 +3,11 @@ function filterDrinks(eco, kosch) {
   var drinks = [];
   drinksInfo = beerInfoList2.filter(function(item) {
     var state = true;
-    if(eco) {
-      if(item.ekologisk != 1) { state = false; }
+    if (eco) {
+      if (item.ekologisk != 1) { state = false; }
     }
-    if(kosch) {
-      if(item.koscher != 1) { state = false; }
+    if (kosch) {
+      if (item.koscher != 1) { state = false; }
     }
     return state;
   });
@@ -20,5 +20,24 @@ function filterDrinks(eco, kosch) {
     }
   }
   getInventory(drinks, drinksInfo)
+}
 
+function searchDrinks(str) {
+  drinks = [];
+  drinksInfo = beerInfoList2.filter(function(item) {
+    if (item.namn.toLowerCase().indexOf(str.toLowerCase()) > -1) {
+      return true;
+    }
+    return false;
+  });
+
+  for (var i = 0; i < drinksInfo.length; i++) {
+    for (var j = 0; j < beerInfoList.length; j++) {
+      if (drinksInfo[i].nr == beerInfoList[j].beer_id) {
+        drinks.push(beerInfoList[j]);
+      }
+    }
+  }
+
+  getInventory(drinks, drinksInfo);
 }
